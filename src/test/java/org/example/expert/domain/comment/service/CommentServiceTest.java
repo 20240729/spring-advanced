6c6d5b10b6e656df9753e82e45960a9741c3f10b
20240729 <yoohyeonsik@gmail.com>
2024-09-13 10:01:45 +0900

    매니저서비스 대충 완

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.service;
 
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
@@ -16,7 +17,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -70,5 +73,32 @@ class CommentServiceTest {
 
         // then
         assertNotNull(result);
+    }
+
+    @Test
+    void comment목록조회(){
+
+        // g
+        /*필요한 것 : 할일아이디, 댓글리포지토리가 제공하는 댓글목록*/
+        long todoId = 1;
+        User user = new User("user1@example.com", "password", UserRole.USER);
+        Todo todo = new Todo("Title", "Contents", "Sunny", user);
+        ReflectionTestUtils.setField(todo, "id", todoId);
+        // 4. 만들었음
+        Comment mockComment = new Comment("contents", user, todo);
+        // 3. 코멘트를 위한 유저, 할일이 필요함
+        List<Comment> comments = List.of(mockComment);
+        // 2. 리스트를 위한 코멘트가 필요함
+        given(commentRepository.findByTodoIdWithUser(todoId)).willReturn(comments);
+        // 1. 댓글리포지토리가 제공하는 댓글목록 만듦, 코멘트 리스트가 필요함
+
+        // w
+        List<CommentResponse> commentResponses = commentService.getComments(todoId);
+        /*이걸 하기 위해 있어야 할 것
+        할일아이디, 댓글리포지토리가 제공하는 댓글목록*/
+
+        // t
+        assertNotNull(commentResponses);
+        assertEquals(1, commentResponses.size());
     }
 }
